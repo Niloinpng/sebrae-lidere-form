6d95243b62b1205f842b88466b8f22742ea36998
Nilo Silva <danilo.silveira.silv@gmail.com>
2025-01-16 11:27:43 -0300

    :sparkles: aprimora validação de dados no formulário com checagem de CPF, e-mail e celular

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,40 +1,98 @@
 import { Infos } from "../types/Infos";
 
 type Error = {
-    [key: string]: string;
-}
+  [key: string]: string;
+};
 
-export const validate = (data: Infos) => {
-     
-    const erros: Error = {};
+const invalidCPF: string[] = [
+  "00000000000",
+  "11111111111",
+  "22222222222",
+  "33333333333",
+  "44444444444",
+  "55555555555",
+  "66666666666",
+  "77777777777",
+  "88888888888",
+  "99999999999",
+];
 
-    if(!data.nome){
-        erros["nome"] = "Preencha seu nome completo";
-    }
+const isEmail = (str: string): boolean => {
+  if (str.length <= 4) return false;
+  const posArroba: number = str.indexOf("@");
+  const posDot: number = str.lastIndexOf(".");
+  return posArroba >= 0 && posDot > posArroba;
+};
 
-    if(!data.cpf){
-        erros["cpf"] = "Informe seu CPF";
-    }
+const isCPF = (strCPF: string): boolean => {
+  strCPF = strCPF.replace(/[^\d]+/g, "");
 
-    if(!data.email){
-        erros["email"] = "Campo não preenchido corretamente";
-    }
+  if (invalidCPF.indexOf(strCPF) >= 0) return false;
 
-    if(!data.celular){
-        erros["celular"] = "Campo não preenchido corretamente";
-    }
+  let soma = 0;
+  let resto: number;
 
-    if(!data.estado){
-        erros["estado"] = "Selecione um estado";
-    }
+  for (let i = 1; i <= 9; i++) {
+    soma += parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+  }
 
-    if(!data.cidade){
-        erros["cidade"] = "Informe sua cidade";
-    }
+  resto = (soma * 10) % 11;
+  resto = resto === 10 || resto === 11 ? 0 : resto;
+  if (resto !== parseInt(strCPF.substring(9, 10))) return false;
 
-    if(!data.aceito){
-        erros["aceito"] = "Aceite os termos";
-    }
+  soma = 0;
+  for (let i = 1; i <= 10; i++) {
+    soma += parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+  }
 
-    return erros;
-} 
+  resto = (soma * 10) % 11;
+  resto = resto === 10 || resto === 11 ? 0 : resto;
+  if (resto !== parseInt(strCPF.substring(10, 11))) return false;
+
+  return true;
+};
+
+const isCelular = (str: string): boolean => {
+  str = str.replace(/[^\d]+/g, "");
+  return str.length === 11;
+};
+
+export const validate = (data: Infos): Error => {
+  const erros: Error = {};
+
+  if (!data.nome) {
+    erros["nome"] = "Preencha seu nome completo";
+  }
+
+  if (!data.cpf) {
+    erros["cpf"] = "Informe seu CPF";
+  } else if (!isCPF(data.cpf)) {
+    erros["cpf"] = "CPF inválido";
+  }
+
+  if (!data.email) {
+    erros["email"] = "Informe seu e-mail";
+  } else if (!isEmail(data.email)) {
+    erros["email"] = "E-mail inválido";
+  }
+
+  if (!data.celular) {
+    erros["celular"] = "Informe seu celular";
+  } else if (!isCelular(data.celular)) {
+    erros["celular"] = "Celular inválido";
+  }
+
+  if (!data.estado) {
+    erros["estado"] = "Selecione um estado";
+  }
+
+  if (!data.cidade) {
+    erros["cidade"] = "Informe sua cidade";
+  }
+
+  if (!data.aceito) {
+    erros["aceito"] = "Aceite os termos";
+  }
+
+  return erros;
+};
